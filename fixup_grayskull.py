e4bb9aec48da947e40f18bfb86f5572208f84a68
sgbaird <sterling.baird@icloud.com>
2021-10-30 14:05:05 -0600

    Create fixup_grayskull.py

---
+++ fixup_grayskull.py
@@ -0,0 +1,10 @@
+"""Touch up the conda recipe from grayskull using conda-souschef."""
+from souschef.recipe import Recipe
+from os.path import join
+
+fpath = join("dist_matrix", "meta.yaml")
+fpath2 = join("scratch", "meta.yaml")
+my_recipe = Recipe(load_file=fpath)
+my_recipe["requirements"]["host"].append("flit")
+my_recipe.save(fpath)
+my_recipe.save(fpath2)
