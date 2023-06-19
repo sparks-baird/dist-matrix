f76de03f73cf7019fa1d8fc9e3f356fef721937e
sgbaird <sterling.baird@utah.edu>
2023-06-19 04:34:25 -0600

    grayskull

--- fixup_grayskull.py
+++ fixup_grayskull.py
@@ -5,7 +5,6 @@ from os.path import join
 fpath = join("dist-matrix", "meta.yaml")
 fpath2 = join("scratch", "meta.yaml")
 my_recipe = Recipe(load_file=fpath)
-# remove
 my_recipe["requirements"]["host"].replace("flit_core >=3.2,<4", "flit")
 my_recipe.save(fpath)
 my_recipe.save(fpath2)
